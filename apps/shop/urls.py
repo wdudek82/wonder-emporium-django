10496976d52856bf765bd2f3435d0e9fc862d926
Wojciech Dudek <wdudek82@gmail.com>
2017-07-07 22:16:16 +0200

    Created stub function views, and templates (for product list, product detail, product list by category)

--- apps/shop/urls.py
+++ apps/shop/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls import url
-from .views import *
+from .views import (product_detail_view, product_list_view, product_list_by_category_view,
+                    ProductDetailView, ProductListByCategoryView)
 
 
 urlpatterns = [
-    url(r'category/(?P<category_id>\d+)/$', ProductsListByCategoryView, name='product_list_by_category'),
-    url(r'product/(?P<product_id>\d+)/$', ProductDetailView, name='product_detail'),
+    url(r'products/$', product_list_view, name='product_list'),
+    url(r'product/(?P<product_id>\d+)/$', product_detail_view, name='product_detail'),
+    url(r'products/category/(?P<category_id>\d+)/$', product_list_by_category_view, name='product_list_by_category'),
+
+    url(r'cbv/products/category/(?P<category_id>\d+)/$', ProductListByCategoryView, name='cbv_product_list_by_category'),
+    url(r'cbv/product/(?P<product_id>\d+)/$', ProductDetailView, name='cbv_product_detail'),
 ]
--- apps/shop/views.py
+++ apps/shop/views.py
@@ -1,7 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Product
 
 
-class ProductsListByCategoryView:
+def product_list_view(request):
+    products = Product.objects.filter(available=True).order_by('id')
+
+    context = {
+        'products': products
+    }
+
+    return render(request, 'shop/product_list.html', context)
+
+
+def product_detail_view(request, product_id=None):
+    product = get_object_or_404(Product, id=product_id, available=True)
+
+    context = {
+        'product': product
+    }
+    return render(request, 'shop/product_detail.html', context)
+
+
+def product_list_by_category_view(request, category_id=None):
+    products = Product.objects.filter(category=category_id, available=True)
+
+    context = {
+        'products': products,
+    }
+    return render(request, 'shop/product_list_by_category.html', context)
+
+
+class ProductListByCategoryView:
     pass
 
 
