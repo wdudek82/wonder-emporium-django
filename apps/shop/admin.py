0f483a1fd59a72e8c6a46489615610d64c869db0
Wojciech Dudek <wdudek82@gmail.com>
2017-07-07 20:57:25 +0200

    Created, and registered, admin models for Category, and Product, added first product images

--- apps/shop/admin.py
+++ apps/shop/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.utils.html import mark_safe
+from .models import Category, Product
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'slug']
+    list_display_links = ['name']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'slug', 'description', 'category', 'get_image', 'price', 'stock', 'available',
+                    'created', 'updated']
+    list_display_links = ['name']
+    list_editable = ['price', 'stock', 'available']
+    prepopulated_fields = {'slug': ('name',)}
+
+    def get_image(self, instance):
+        image = instance.image
+        return mark_safe('<img src ="{}">'.format(image.url)) if image else '-'
+    get_image.short_description = 'image'
+
